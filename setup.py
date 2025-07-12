cb214cf370dde3839f885c73fe65b11e51fe496a
cconejob <cconejob@gmail.com>
2025-07-12 12:27:35 +0200

    upgrade to version 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='stl_constrained_zpc',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=required,  # Use the list from requirements.txt
     entry_points={
